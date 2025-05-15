b98589a72bf2acfdbcb233c68e4c513e5712d194
erisasollova <erisasollova@gmail.com>
2025-05-15 14:42:22 +0200

    Rregullimi i Sintakses

--- test.c
+++ test.c
@@ -4,6 +4,27 @@
 
 int counter = 0;
 pthread_mutex_t lock;
+
+void* increment_counter(void* arg) {
+    for(int i = 0; i < 100000; i++) {
+        pthread_mutex_lock(&lock);
+        counter++;
+        pthread_mutex_unlock(&lock);
+    }
+    return NULL;
+}
+
 int main() {
+    pthread_t t1, t2;
     pthread_mutex_init(&lock, NULL);
+
+    pthread_create(&t1, NULL, increment_counter, NULL);
+    pthread_create(&t2, NULL, increment_counter, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("Vlera përfundimtare e counter: %d\n", counter);
+    pthread_mutex_destroy(&lock);
+    return 0;
 }
