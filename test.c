9da7dba4a319946570c7b5f1dfd9ec6e6f5b2b4a
ErinaMustafa <erina.mustafa@student.uni-pr.edu>
2025-05-15 13:37:46 +0200

    ImplementIimi i funksionit increment_counter dhe krijimi dy thread-e për inkrementim

--- test.c
+++ test.c
@@ -6,4 +6,16 @@ int counter = 0;
 pthread_mutex_t lock;
 int main() {
     pthread_mutex_init(&lock, NULL);
+
+void* increment_counter(void* arg) {
+    for(int i = 0; i < 100000; i++) {
+        pthread_mutex_lock(&lock);
+        counter++;
+        pthread_mutex_unlock(&lock);
+    }
+    return NULL;
+}
+
+    pthread_create(&t1, NULL, increment_counter, NULL);
+    pthread_create(&t2, NULL, increment_counter, NULL);
 }
